5459c789b409ba06d0970c2875f2f217701c797f
Tobias P. Schmidt <tobias.patrick.schmidt@gmail.com>
2016-02-09 10:45:37 +0100

    highlighting configuration class integrated for syntax highlighting

--- plantuml.eclipse.ui/src/plantuml/eclipse/ui/PumlUiModule.java
+++ plantuml.eclipse.ui/src/plantuml/eclipse/ui/PumlUiModule.java
@@ -4,6 +4,9 @@
 package plantuml.eclipse.ui;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingConfiguration;
+
+import plantuml.eclipse.ui.highlighting.PumlHighlightingConfiguration;
 
 /**
  * Use this class to register components to be used within the IDE.
@@ -11,5 +14,10 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 public class PumlUiModule extends plantuml.eclipse.ui.AbstractPumlUiModule {
 	public PumlUiModule(AbstractUIPlugin plugin) {
 		super(plugin);
+	}
+	
+	public Class<? extends IHighlightingConfiguration> 
+    bindIHighlightingConfiguration() {
+     return PumlHighlightingConfiguration.class;
 	}
 }
